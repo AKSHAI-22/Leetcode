53cc30d9950a3091e048f149dd3041773402be8c
AKSHAI <akshairaj22@gmail.com>
2025-08-29 09:30:07 +0530

    Create 504. Base 7.py
    Given an integer num, return a string of its base 7 representation.
    
     
    
    Example 1:
    
    Input: num = 100
    Output: "202"
    Example 2:
    
    Input: num = -7
    Output: "-10"
     
    
    Constraints:
    
    -107 <= num <= 107

---
+++ 504. Base 7.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def convertToBase7(self, num):
+        if num == 0:
+            return "0"
+
+        neg = num < 0         
+        num = abs(num)        
+
+        res = ""
+        while num > 0:
+            res = str(num % 7) + res
+            num //= 7
+
+        if neg:
+            res = "-" + res
+        return res
+
+
+        
